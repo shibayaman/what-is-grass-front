03cbf4115d2eb5e403a1baf133858f55d735c695
shibayaman <shibayaman7374@gmail.com>
2021-07-22 14:01:09 +0900

    Add マイトップページ

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,25 +1,23 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useSelector } from '@what-is-grass/shared';
 import Layout from '../components/Layout';
-import { greet } from '@what-is-grass/shared';
-import { useState, useEffect } from 'react';
 import Login from '../components/Login';
 
 const IndexPage: React.FC = () => {
-  const [user, setUser] = useState('World');
+  const user = useSelector((state) => state.auth.user);
+  const router = useRouter();
 
   useEffect(() => {
-    const callApi = async () => {
-      const res = await fetch('/user');
-      const data = await res.json();
-      setUser(data.username);
-    };
-
-    callApi();
-  }, []);
+    if (user !== null) {
+      router.replace('/my-top');
+    }
+  }, [user, router]);
 
   return (
     <Layout title="Home">
-      <h1 className="text-xl border-black border-2">{greet(user)}</h1>
+      <h1 className="text-xl border-black border-2">Top Page</h1>
       <p>
         <Link href="/about">
           <a>About</a>
---
+++ packages/web/pages/my-top.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import Layout from '../components/Layout';
+
+const MyTopPage: FC = () => {
+  return (
+    <Layout>
+      <div>My Top Page</div>
+    </Layout>
+  );
+};
+
+export default MyTopPage;
