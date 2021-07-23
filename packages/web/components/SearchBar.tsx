6f613dbc2ca819ba74c383da505fb7e33da5dc12
shibayaman <shibayaman7374@gmail.com>
2021-07-24 01:11:41 +0900

    Add 検索画面でログイン済みの場合のみ質問できる機能

--- packages/web/components/SearchBar.tsx
+++ packages/web/components/SearchBar.tsx
@@ -8,7 +8,6 @@ import {
 } from '@what-is-grass/shared';
 import { useEffect } from 'react';
 import { shallowEqual } from 'react-redux';
-import { useRouter } from 'next/router';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
@@ -32,7 +31,6 @@ const searchFormSchema = yup.object({
 });
 
 const SearchBar: React.FC<Props> = (props) => {
-  const router = useRouter();
   const dispatch = useDispatch();
   const latestSearchRequest = useSelector(
     (state) => state.questions.latestSearchRequest,
@@ -43,7 +41,7 @@ const SearchBar: React.FC<Props> = (props) => {
     skip: latestSearchRequest === null,
   });
 
-  const { register, handleSubmit, getValues } = useForm<FormValue>({
+  const { register, handleSubmit } = useForm<FormValue>({
     defaultValues: {
       keyword: latestSearchRequest?.keyword ?? '',
       languageId: latestSearchRequest?.language_id ?? 1,
@@ -74,60 +72,43 @@ const SearchBar: React.FC<Props> = (props) => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="search-box">
-          <div className="search-input">
-            <input
-              name="keyword"
-              ref={register}
-              placeholder="なんかを聞きたいの？"
-              aria-label="なんかを聞きたいの？"
-            />
-            <input type="submit" value="検索" disabled={isLoading} />
-          </div>
-        </div>
-        <div>
-          <select name="languageId" ref={register} className="search-lauguage">
-            <option value="1">日本語</option>
-            <option value="2">英語</option>
-          </select>
-        </div>
-        <div>
-          <select name="sortId" ref={register} className="sort">
-            <option value="1">日付</option>
-            <option value="2">役立Count</option>
-            <option value="3">回答者数</option>
-          </select>
-        </div>
-        <div>
-          <select
-            name="includeNoAnswer"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="search-box">
+        <div className="search-input">
+          <input
+            name="keyword"
             ref={register}
-            className="include_no_answer"
-          >
-            <option value="1">全て</option>
-            <option value="2">回答あり</option>
-            <option value="3">回答なし</option>
-          </select>
+            placeholder="なんかを聞きたいの？"
+            aria-label="なんかを聞きたいの？"
+          />
+          <input type="submit" value="検索" disabled={isLoading} />
         </div>
-      </form>
-
-      <button
-        className="float-right"
-        type="button"
-        onClick={() => {
-          router.push({
-            pathname: '/new-question',
-            query: {
-              keyword: getValues('keyword'),
-            },
-          });
-        }}
-      >
-        質問
-      </button>
-    </div>
+      </div>
+      <div>
+        <select name="languageId" ref={register} className="search-lauguage">
+          <option value="1">日本語</option>
+          <option value="2">英語</option>
+        </select>
+      </div>
+      <div>
+        <select name="sortId" ref={register} className="sort">
+          <option value="1">日付</option>
+          <option value="2">役立Count</option>
+          <option value="3">回答者数</option>
+        </select>
+      </div>
+      <div>
+        <select
+          name="includeNoAnswer"
+          ref={register}
+          className="include_no_answer"
+        >
+          <option value="1">全て</option>
+          <option value="2">回答あり</option>
+          <option value="3">回答なし</option>
+        </select>
+      </div>
+    </form>
   );
 };
 
--- packages/web/pages/search.tsx
+++ packages/web/pages/search.tsx
@@ -1,14 +1,27 @@
 import Layout from '../components/Layout';
 import { useState } from 'react';
+import Link from 'next/link';
 import SearchBar from '../components/SearchBar';
 import IndexItem from '../components/IndexItem';
-import { Index } from '@what-is-grass/shared';
+import { Index, useSelector } from '@what-is-grass/shared';
 
 const Search: React.FC = () => {
   const [questions, setQuestions] = useState<Index[]>([]);
+  const keyword = useSelector(
+    (state) => state.questions.latestSearchRequest?.keyword ?? null
+  );
+  const user = useSelector((state) => state.auth.user);
 
   const updateQuestion = (questions: Index[]) => {
     setQuestions(questions);
+  };
+
+  const getNewQuestionUrl = () => {
+    let url = '/new-question';
+    if (keyword) {
+      url += `?keyword=${keyword}`;
+    }
+    return url;
   };
 
   return (
@@ -19,6 +32,18 @@ const Search: React.FC = () => {
       {questions.map((question) => (
         <IndexItem key={question.index_id} question={question} />
       ))}
+      {user ? (
+        <div>
+          言葉が見つかりませんか？{' '}
+          <Link href={getNewQuestionUrl()}>こちら</Link>
+          から{keyword && `${keyword}について`}新しく質問しましょう
+        </div>
+      ) : (
+        <div>
+          言葉が見つかりませんか？ <Link href="/">ログイン</Link>して
+          {keyword && `${keyword}について`}新しく質問しましょうて
+        </div>
+      )}
     </Layout>
   );
 };
