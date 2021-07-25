b6812109e277e16a44136166ed0430745bb600da
shibayaman <shibayaman7374@gmail.com>
2021-07-25 20:04:56 +0900

    Add マイトップページをプライベートに

--- packages/web/pages/my-top.tsx
+++ packages/web/pages/my-top.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
 import Layout from '../components/Layout';
+import PrivatePage from '../components/PrivatePage';
 
 const MyTopPage: FC = () => {
   return (
-    <Layout>
-      <div>My Top Page</div>
-    </Layout>
+    <PrivatePage redirectTo="/">
+      <Layout>
+        <div>My Top Page</div>
+      </Layout>
+    </PrivatePage>
   );
 };
 
