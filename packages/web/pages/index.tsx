c08e1c58ce9e260e0af3be127f0a676935dd5ee1
shibayaman <shibayaman7374@gmail.com>
2021-07-25 19:54:28 +0900

    Add ログインしてたらリダイレクトするHOC

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,31 +1,22 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { useSelector } from '@what-is-grass/shared';
 import Layout from '../components/Layout';
 import Login from '../components/Login';
+import GuestOnlyPage from '../components/GuestOnlyPage';
 
 const IndexPage: React.FC = () => {
-  const user = useSelector((state) => state.auth.user);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (user !== null) {
-      router.replace('/my-top');
-    }
-  }, [user, router]);
-
   return (
-    <Layout title="Home">
-      <h1 className="text-xl border-black border-2">Top Page</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-      <br />
-      <Login />
-    </Layout>
+    <GuestOnlyPage redirectTo="/my-top">
+      <Layout title="Home">
+        <h1 className="text-xl border-black border-2">Top Page</h1>
+        <p>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </p>
+        <br />
+        <Login />
+      </Layout>
+    </GuestOnlyPage>
   );
 };
 
