2f15b0e0d7d527afb25b2e14b64ad58aec6f1639
shibayaman <shibayaman7374@gmail.com>
2021-07-27 22:38:52 +0900

    Add fowradRef

--- packages/web/components/SelectBox.tsx
+++ packages/web/components/SelectBox.tsx
@@ -1,24 +1,27 @@
 import classNames from 'classnames';
-import { FC } from 'react';
+import { forwardRef } from 'react';
 
 type Props = Omit<JSX.IntrinsicElements['select'], 'size'> & {
   size: 'xs' | 'base' | 'lg';
+  name: string;
 };
 
-const SelectContainer: FC<Props> = ({ size, children, ...props }) => {
-  const baseStyle =
-    'px-2 py-1 border border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-green-400';
-  const styles = classNames(baseStyle, {
-    'text-xs': size === 'xs',
-    'text-base': size === 'base',
-    'text-xl': size === 'lg',
-  });
+const SelectContainer = forwardRef<HTMLSelectElement, Props>(
+  ({ size, name, children, ...props }, ref) => {
+    const baseStyle =
+      'px-2 py-1 border border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-green-400';
+    const styles = classNames(baseStyle, {
+      'text-xs': size === 'xs',
+      'text-base': size === 'base',
+      'text-xl': size === 'lg',
+    });
 
-  return (
-    <select className={styles} {...props}>
-      {children}
-    </select>
-  );
-};
+    return (
+      <select className={styles} ref={ref} name={name} {...props}>
+        {children}
+      </select>
+    );
+  }
+);
 
 export default SelectContainer;
--- packages/web/components/TextInput.tsx
+++ packages/web/components/TextInput.tsx
@@ -1,9 +1,10 @@
 import classNames from 'classnames';
-import { FC } from 'react';
+import { forwardRef } from 'react';
 
 type Props = Omit<JSX.IntrinsicElements['input'], 'size'> & {
   size: 'xs' | 'base' | 'lg';
   width: 'xs' | 'base' | 'lg';
+  name: string;
 };
 
 const sizeSchemes = {
@@ -12,16 +13,26 @@ const sizeSchemes = {
   lg: 50,
 };
 
-const TextInput: FC<Props> = ({ size, width, ...props }) => {
-  const baseStyle =
-    'border border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-green-400';
-  const styles = classNames(baseStyle, {
-    'text-xs py-1 px-5': size === 'xs',
-    'text-base py-2 px-5': size === 'base',
-    'text-xl py-3 px-5': size === 'lg',
-  });
+const TextInput = forwardRef<HTMLInputElement, Props>(
+  ({ size, width, name, ...props }, ref) => {
+    const baseStyle =
+      'border border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-green-400';
+    const styles = classNames(baseStyle, {
+      'text-xs py-1 px-5': size === 'xs',
+      'text-base py-2 px-5': size === 'base',
+      'text-xl py-3 px-5': size === 'lg',
+    });
 
-  return <input className={styles} size={sizeSchemes[width]} {...props} />;
-};
+    return (
+      <input
+        className={styles}
+        size={sizeSchemes[width]}
+        ref={ref}
+        name={name}
+        {...props}
+      />
+    );
+  }
+);
 
 export default TextInput;
