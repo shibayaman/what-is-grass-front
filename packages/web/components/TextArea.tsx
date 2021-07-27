47853ce9ed9fa60a2ccb452bdc896629733d0d8c
shibayaman <shibayaman7374@gmail.com>
2021-07-27 23:02:40 +0900

    Add テキストインプットはエラーの時赤くなる

--- packages/web/components/TextArea.tsx
+++ packages/web/components/TextArea.tsx
@@ -4,14 +4,17 @@ import { forwardRef } from 'react';
 type Props = JSX.IntrinsicElements['textarea'] & {
   size: 'xs' | 'base' | 'lg';
   name: string;
+  isError?: boolean;
   resize?: 'none' | 'vertical' | 'horizontal' | 'both';
 };
 
 const TextArea = forwardRef<HTMLTextAreaElement, Props>(
-  ({ size, name, resize = 'both', ...props }, ref) => {
-    const baseStyle =
-      'border leading-relaxed border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-green-400';
-    const styles = classNames(baseStyle, {
+  ({ size, name, isError = false, resize = 'both', ...props }, ref) => {
+    const baseStyle = 'leading-relaxed rounded focus:outline-none focus:ring-1';
+    const borderStyle = isError
+      ? 'border-2 border-red-600 focus:ring-red-400'
+      : 'border border-gray-400 focus:ring-green-400';
+    const styles = classNames(baseStyle, borderStyle, {
       'text-xs py-1 px-1': size === 'xs',
       'text-base py-2 px-2': size === 'base',
       'text-xl py-3 px-3': size === 'lg',
--- packages/web/components/TextInput.tsx
+++ packages/web/components/TextInput.tsx
@@ -5,6 +5,7 @@ type Props = Omit<JSX.IntrinsicElements['input'], 'size'> & {
   size: 'xs' | 'base' | 'lg';
   width: 'xs' | 'base' | 'lg';
   name: string;
+  isError?: boolean;
 };
 
 const sizeSchemes = {
@@ -14,10 +15,12 @@ const sizeSchemes = {
 };
 
 const TextInput = forwardRef<HTMLInputElement, Props>(
-  ({ size, width, name, ...props }, ref) => {
-    const baseStyle =
-      'border border-gray-400 rounded focus:outline-none focus:ring-1 focus:ring-green-400';
-    const styles = classNames(baseStyle, {
+  ({ size, width, name, isError = false, ...props }, ref) => {
+    const baseStyle = 'rounded focus:outline-none focus:ring-1';
+    const borderStyle = isError
+      ? 'border-2 border-red-600 focus:ring-red-400'
+      : 'border border-gray-400 focus:ring-green-400';
+    const styles = classNames(baseStyle, borderStyle, {
       'text-xs py-1 px-5': size === 'xs',
       'text-base py-2 px-5': size === 'base',
       'text-xl py-3 px-5': size === 'lg',
