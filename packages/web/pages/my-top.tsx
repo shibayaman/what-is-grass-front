da2efa8ccd5d4e5e22ba8ed4965dda6be26845d4
shibayaman <shibayaman7374@gmail.com>
2021-09-16 07:01:59 +0900

    Add 最近の質問を表示

--- packages/web/pages/my-top.tsx
+++ packages/web/pages/my-top.tsx
@@ -1,4 +1,9 @@
-import { communityTranslator, useSelector } from '@what-is-grass/shared';
+import {
+  communityTranslator,
+  useGetUserIndicesQuery,
+  useSelector,
+} from '@what-is-grass/shared';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FC, Fragment } from 'react';
 import Button from '../components/Button';
@@ -9,18 +14,23 @@ import Tag from '../components/Tag';
 
 const MyTopPage: FC = () => {
   const user = useSelector((state) => state.auth.user);
+
+  const { data: recentUserIndices } = useGetUserIndicesQuery({
+    index_limit: 5,
+  });
+
   const router = useRouter();
 
   return (
     <PrivatePage redirectTo="/">
       <Layout>
-        <div className="grid grid-cols-9 gap-4 p-4">
-          <div className="col-span-3 flex justify-center">
-            {user && (
-              <Card className="w-80" withShadow={false}>
-                <div className="mb-4 flex flex-col gap-2 justify-center items-center">
+        {user && (
+          <div className="grid grid-cols-9 gap-4 p-4">
+            <div className="col-span-3 flex flex-col gap-4 items-center min-w-max">
+              <Card withShadow={false}>
+                <div className="mb-4 flex flex-col gap-2 items-center">
                   <img src="/sample-user-icon.png" width="80px"></img>
-                  <h2 className="text-lg">{user.username}</h2>
+                  <h2 className="text-xl">{user.username}</h2>
                   <p>
                     {user.community_tags.map((community) => (
                       <Fragment key={community.id}>
@@ -61,10 +71,31 @@ const MyTopPage: FC = () => {
                   プロフィールを変更する
                 </Button>
               </Card>
-            )}
+              <Card className="max-w-xs" withShadow={false}>
+                <h2 className="text-xl">最近質問した見出し</h2>
+                <div className="p-4 flex flex-col gap-4">
+                  {recentUserIndices &&
+                    (recentUserIndices.length === 0 ? (
+                      <p>最近の質問はありません</p>
+                    ) : (
+                      recentUserIndices.map((index) => (
+                        <Card key={index.id} className="">
+                          <Link href={`/answers/${index.id}`}>
+                            <a className="text-lg text-green-600">
+                              {index.index}
+                            </a>
+                          </Link>
+                          <br />
+                          <p>{index.answer_count}件の回答があります</p>
+                        </Card>
+                      ))
+                    ))}
+                </div>
+              </Card>
+            </div>
+            <div className="col-span-6">おすすめ見出し</div>
           </div>
-          <div className="col-span-6">おすすめ見出し</div>
-        </div>
+        )}
       </Layout>
     </PrivatePage>
   );
